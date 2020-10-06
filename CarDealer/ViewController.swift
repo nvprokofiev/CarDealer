67783ce25dffbb039e58255402226712dbe3bd4f
Nikolai Prokofev <nvprokofiev@gmail.com>
2020-10-05 22:24:56 -0400

    Initial Commit

---
+++ CarDealer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarDealer
+//
+//  Created by Nikolai Prokofev on 2020-10-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
