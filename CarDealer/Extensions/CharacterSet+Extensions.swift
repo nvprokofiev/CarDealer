ff970d2f4ebe9c793dfd8e2981600c777accbc6d
Nikolai Prokofev <nvprokofiev@gmail.com>
2020-10-06 23:28:09 -0400

    Add CharascterSet+Extensions to git index

---
+++ CarDealer/Extensions/CharacterSet+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterSet+EXTENSIONS.swift
+//  CarDealer
+//
+//  Created by Nikolai Prokofev on 2020-10-06.
+//
+
+import Foundation
+
+extension CharacterSet {
+    
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@"
+        let subDelimitersToEncode = "!$&'()*+,;="
+        
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        return allowed
+    }()
+}
